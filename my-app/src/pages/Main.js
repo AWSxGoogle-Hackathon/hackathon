1bb0b3420a7ec33b2270cbe9769463f3f5aee387
andy796 <andywu482@gmail.com>
2022-09-10 20:30:55 +1000

    Sidebar template

---
+++ my-app/src/pages/Main.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import '../App.css';
+import Header from '../components/Header';
+
+
+const Main = () => {
+  return (
+    <>
+        <Header/>
+    </>
+  );
+}
+
+export default Main
