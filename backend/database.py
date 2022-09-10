4d99939c21d0648ac0b6ef8c2adcc95ae213dbe4
andy796 <andywu482@gmail.com>
2022-09-10 18:11:30 +1000

    Basic template setup

---
+++ backend/database.py
@@ -0,0 +1,14 @@
+'''this file contains functions to connect and manage the database'''
+from pymongo import MongoClient
+
+def access_db():
+    pass
+
+def add_to_db():
+    pass
+
+def delete_in_db(render_id, api_key):
+    pass
+
+def get_dashboard(api_key):
+    pass
